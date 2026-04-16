a393e761186658fb0b069f7c64febe7b7b48344f
elhadjifaliloufall25-dotcom <elhadjifaliloufall25@gmail.com>
2026-04-16 15:09:09 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState, useEffect } from "react";
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabase = createClient(
-  'https://chhiiebjwzntnkhgcwae.supabase.co',
-  sb_publishable_FnoE82VwA7gYKhdolBXfWQ_jivbJ8W5
-);
+import { useState } from "react";
 
 const C = {
   terra:"#C4572A",gold:"#E8A020",green:"#1A7A4A",
